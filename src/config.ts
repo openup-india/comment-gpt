cfe2f6f95c185e94260f24f02ed3aaf63a83b13d
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-07-10 21:27:05 +0530

    Project Init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+  OPEN_AI_KEY: process.env.OPEN_AI_KEY,
+  ENV: process.env.ENVIRONMENT,
+};
---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  console.log("Comment GPT started");
+  vscode.commands.executeCommand("comment-gpt.startCommentGPT");
+
+  let disposable = vscode.commands.registerCommand(
+    "comment-gpt.startCommentGPT",
+    () => {
+      vscode.window.showInformationMessage("Comment GPT is Running");
+
+      vscode.workspace.onDidChangeTextDocument(
+        (e: vscode.TextDocumentChangeEvent) => {
+          if (e.document.isDirty) {
+            console.debug(e.document.getText());
+          }
+        }
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
