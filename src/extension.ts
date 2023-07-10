e4da3690bfe591b976dbebe69b49987a650217c3
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-07-10 21:29:45 +0530

    Added File Text Change Listener

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log("Comment GPT started");
+  // console.log("Comment GPT started");
   vscode.commands.executeCommand("comment-gpt.startCommentGPT");
 
   let disposable = vscode.commands.registerCommand(
