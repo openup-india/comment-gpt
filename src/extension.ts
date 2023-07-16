ed843dbc4ce4ad6fbd69bb6c9296a13830510c74
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-07-16 22:25:56 +0530

    Integrated Javascript Parser in Extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { jsparser } from "./parser/javascript.parser";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.workspace.onDidChangeTextDocument(
         (e: vscode.TextDocumentChangeEvent) => {
           if (e.document.isDirty) {
-            console.debug(e.document.getText());
+            console.debug(jsparser(e.document.getText()));
           }
         }
       );
