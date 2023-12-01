cd1ed0f0eb8dc9e3d61d78c9acb31c2da2982a7a
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-12-01 15:24:28 +0530

    Added Env Support for config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+
+dotenv.config({ path: path.join(__dirname, '../.env') })
+
+const envVars = process.env
+
 export default {
-  OPENAI_API_KEY: process.env.OPEN_AI_KEY,
-  ENV: process.env.ENVIRONMENT,
+  OPENAI_API_KEY: envVars.OPEN_AI_KEY,
+  ENV: envVars.ENVIRONMENT,
 }
